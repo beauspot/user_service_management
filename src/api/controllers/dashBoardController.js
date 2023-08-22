31984f3323c7078391ce166e0444eabc8db40290
beauspot <beauspot@outlook.com>
2023-08-22 11:25:42 +0100

    the dashboard controller

---
+++ src/api/controllers/dashBoardController.js
@@ -0,0 +1,12 @@
+import AsyncHandler from "express-async-handler";
+import { StatusCodes } from "http-status-codes";
+
+export const dashboardCtrl = AsyncHandler((req, res) => {
+  const username = req.user.username;
+  console.log(username);
+  if (username) {
+    res.send(`Welcome to Your Dashboard, ${username}!`);
+  } else {
+    res.status(StatusCodes.UNAUTHORIZED).json("Not logged in");
+  }
+});
