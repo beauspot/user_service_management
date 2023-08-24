35ed14c6d329e05dded92b0181f6a744640c41aa
beauspot <beauspot@outlook.com>
2023-08-24 19:19:13 +0100

    added cors options

--- src/api/index.js
+++ src/api/index.js
@@ -35,6 +35,7 @@ dotenv.config();
 
 const app = express();
 app.set("trust proxy", false);
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
@@ -42,9 +43,15 @@ const limiter = rateLimit({
   legacyHeaders: false,
 });
 
+const corsOptions = {
+  origins: ["https://user-management-service-api.onrender.com"],
+  methods: "GET, POST",
+  credentials: true,
+};
+
 app.use(morgan("dev"));
 app.use(limiter);
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(express.json());
 app.use(
