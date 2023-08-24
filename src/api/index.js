474bae17b0263730746352af934df2dfc288d60b
beauspot <beauspot@outlook.com>
2023-08-24 19:29:44 +0100

    took out the limiter

--- src/api/index.js
+++ src/api/index.js
@@ -36,7 +36,7 @@ dotenv.config();
 const app = express();
 app.set("trust proxy", true);
 
-const limiter = rateLimit({
+/* const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true,
@@ -47,11 +47,11 @@ const corsOptions = {
   origins: ["https://user-management-service-api.onrender.com"],
   methods: "GET, POST",
   credentials: true,
-};
+}; */
 
 app.use(morgan("dev"));
-app.use(limiter);
-app.use(cors(corsOptions));
+// app.use(limiter);
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(
