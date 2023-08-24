547aaee3119aac4820662b59a7489c44b25c9e99
beauspot <beauspot@outlook.com>
2023-08-24 18:58:45 +0100

    Added the trust proxy configuration.

--- src/api/index.js
+++ src/api/index.js
@@ -14,6 +14,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 
+app.set("trust proxy", true);
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
